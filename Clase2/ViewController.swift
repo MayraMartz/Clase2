f2d2a1b97cddb05953afde9316b4845a00440ec5
ctemdn <telematica.ctemdn@sedena.gob.mx>
2020-05-19 08:54:59 -0500

    Initial Commit

---
+++ Clase2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clase2
+//
+//  Created by ERICK ORTEGA LAZCANO on 19/05/20.
+//  Copyright © 2020 SEDENA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
