0352f6c28da4448f1d8914e642d01a8dc2f39625
ctemdn <telematica.ctemdn@sedena.gob.mx>
2020-05-19 09:17:13 -0500

    [MMM] Se agrego print de saludo

---
+++ Clase2/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  Clase2
+//
+//  Created by ERICK ORTEGA LAZCANO on 19/05/20.
+//  Copyright © 2020 SEDENA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
